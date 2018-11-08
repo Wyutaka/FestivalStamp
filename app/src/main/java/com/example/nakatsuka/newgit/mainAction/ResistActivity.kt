a95de8aa092c1f0577165c85a615e7bbc8d0a4d4
takeya <taked137@gmail.com>
2018-11-08 17:43:29 +0900

    ユーザー登録のAPI実装

--- app/src/main/java/com/example/nakatsuka/newgit/mainAction/ResistActivity.kt
+++ app/src/main/java/com/example/nakatsuka/newgit/mainAction/ResistActivity.kt
@@ -11,12 +11,12 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import com.example.nakatsuka.newgit.R
+import com.example.nakatsuka.newgit.mainAction.controller.api.ApiController
 import kotlinx.android.synthetic.main.activity_resist.*
-import java.util.*
 
 class ResistActivity : AppCompatActivity() {
 
-    lateinit private var inputMethodManager:InputMethodManager
+    lateinit private var inputMethodManager: InputMethodManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,56 +32,85 @@ class ResistActivity : AppCompatActivity() {
         //ユーザー名の登録とUUIDの作成、またその保存
         send_name.setOnClickListener {
             val userName: String = userName.editableText.toString()
-            var nameLength: Int = userName.length
-
 
             AlertDialog.Builder(this)
                     .setTitle("確認")
                     .setMessage(userName + "で登録します")
                     .setPositiveButton("OK") { _, _ ->
-                        if (connection) {
-                            AlertDialog.Builder(this)
-                                    .setTitle("通信エラー")
-                                    .setMessage("もう一度やり直してください")
-                                    .setPositiveButton("OK") { _, _ ->
-                                    }
-                                    .show()
-                        } else if (nameLength < 5) {
-                            AlertDialog.Builder(this)
-                                    .setTitle("エラー")
-                                    .setMessage("5文字以上で入力してください")
-                                    .setPositiveButton("OK") { _, _ ->
-                                    }
-                                    .show()
-                        } else if (used) {
-                            AlertDialog.Builder(this)
-                                    .setTitle("エラー")
-                                    .setMessage("その名前は使用済みです")
-                                    .setPositiveButton("OK") { _, _ ->
-                                    }
-                                    .show()
-                        } else {
-                            AlertDialog.Builder(this)
-                                    .setTitle("登録完了")
-                                    .setPositiveButton("OK") { _, _ ->
 
-                                        val stringUUID = UUID.randomUUID().toString()
-                                        Log.d("UUID",stringUUID)
-                                        val prefer:SharedPreferences = getSharedPreferences("prefer", Context.MODE_PRIVATE)
-                                        val editor: SharedPreferences.Editor =prefer.edit()
-                                        editor.putString("UUID",stringUUID)
-                                        editor.putString("USERNAME",userName)
-                                        Log.d("prefer","prefer")
-                                        editor.commit()
-                                        val APITest = APITest()
-                                        APITest.SetandPostUserJSON(userName, Build.DEVICE,stringUUID)
-                                        val intent = Intent(this, MainActivity::class.java)
-                                        intent.putExtra("APITest", APITest)
-                                        startActivity(intent)
+
+                        //ここにprogressBar
+
+                        val mApiController = ApiController()
+
+                        //val APITest = APITest()
+                        //APITest.device = Build.DEVICE
+                        var stringUUID = ""
+
+                        //TODO 実装例
+                        mApiController.registerUser(userName, Build.DEVICE, Build.VERSION.RELEASE) { response ->
+                            when (response.code()) {
+                                200 -> {
+                                    AlertDialog.Builder(this)
+                                            .setTitle("登録完了")
+                                            .setPositiveButton("OK") { _, _ ->
+
+                                            }
+                                            .show()
+                                    response.body()?.let {
+                                        stringUUID = it.uuid
+
+                                        //APITest.UUID = it.uuid
+                                        //APITest.userName = userName
+
+                                        Log.d("registerUser", "${response.body()}, stringUUID: $stringUUID")
                                     }
-                                    .show()
+
+                                    Log.d("UUID", stringUUID)
+                                    val prefer: SharedPreferences = getSharedPreferences("prefer", Context.MODE_PRIVATE)
+                                    val editor: SharedPreferences.Editor = prefer.edit()
+                                    editor.putString("UUID", stringUUID)
+                                    editor.putString("USERNAME", userName)
+                                    editor.commit()
+                                    val intent = Intent(this, MainActivity::class.java)
+
+                                    //intent.putExtra("APITest", APITest)
+
+                                    startActivity(intent)
+                                }
+                                400 -> {
+                                    AlertDialog.Builder(this)
+                                            .setTitle("エラー")
+                                            .setMessage("5文字以上で入力してください")
+                                            .setPositiveButton("OK") { _, _ ->
+                                            }
+                                            .show()
+                                    Log.e("registerUser", "${response.code()}, ${response.body()}")
+                                }
+                                409 -> {
+                                    AlertDialog.Builder(this)
+                                            .setTitle("エラー")
+                                            .setMessage("その名前は使用済みです")
+                                            .setPositiveButton("OK") { _, _ ->
+                                            }
+                                            .show()
+
+                                }
+                                500 -> {
+
+                                    AlertDialog.Builder(this)
+                                            .setTitle("通信エラー")
+                                            .setMessage("もう一度やり直してください")
+                                            .setPositiveButton("OK") { _, _ ->
+                                            }
+                                            .show()
+                                }
+                            }
                         }
+
+
                     }
+
                     .setNegativeButton("cancel") { _, _ ->
                     }
 
@@ -91,9 +120,8 @@ class ResistActivity : AppCompatActivity() {
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        inputMethodManager.hideSoftInputFromWindow(for_focus.windowToken,InputMethodManager.HIDE_NOT_ALWAYS)
+        inputMethodManager.hideSoftInputFromWindow(for_focus.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
         for_focus.requestFocus()
         return super.onTouchEvent(event)
     }
-
 }
