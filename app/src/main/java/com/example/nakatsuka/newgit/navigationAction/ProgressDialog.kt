667a40dde8420808664db9842579175cf2490de6
takeya <taked137@gmail.com>
2018-11-12 23:30:25 +0900

    ボタンを押したときのダイアログを作りま

---
+++ app/src/main/java/com/example/nakatsuka/newgit/navigationAction/ProgressDialog.kt
@@ -0,0 +1,122 @@
+package com.example.nakatsuka.newgit.navigationAction
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.os.Bundle
+import android.os.Handler
+import android.support.v4.app.DialogFragment
+import android.support.v4.app.FragmentManager
+import android.support.v7.app.AlertDialog
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.example.nakatsuka.newgit.R
+
+class ProgressDialog : DialogFragment() {
+
+    var mProgressBar: ProgressBar? = null
+    private var mProgressMessage: TextView? = null
+    private var mStartedShowing: Boolean = false
+    private var mStartMillisecond: Long = 0
+    private var mStopMillisecond: Long = 0
+
+    private var mMessage: String? = null
+
+    @SuppressLint("InflateParams")
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        mMessage = arguments!!.getString("message")
+
+        val builder = AlertDialog.Builder(activity!!)
+        val inflater = activity!!.layoutInflater
+
+        val inf = inflater.inflate(R.layout.progressdialog, null)
+
+        inf.findViewById<Button>(R.id.exitButton).setOnClickListener {
+            cancel()
+        }
+
+
+        builder.setView(inf)
+
+        return builder.create()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mProgressBar = dialog.findViewById(R.id.progress)
+        mProgressMessage = dialog.findViewById(R.id.progress_message)
+        mProgressMessage!!.text = mMessage
+    }
+
+    override fun show(manager: FragmentManager, tag: String) {
+        mStartMillisecond = System.currentTimeMillis()
+        mStartedShowing = false
+        mStopMillisecond = java.lang.Long.MAX_VALUE
+
+        val handler = Handler()
+        handler.postDelayed({
+            if (mStopMillisecond > System.currentTimeMillis()) {
+                showDialogAfterDelay(manager, tag)
+            }
+        }, DELAY_MILLISECOND.toLong())
+    }
+
+    private fun showDialogAfterDelay(manager: FragmentManager, tag: String) {
+        mStartedShowing = true
+        super.show(manager, tag)
+    }
+
+    private fun cancel() {
+        mStopMillisecond = System.currentTimeMillis()
+
+        if (mStartedShowing) {
+            if (mProgressBar != null) {
+                cancelWhenShowing()
+            } else {
+                cancelWhenNotShowing()
+            }
+        }
+    }
+
+    private fun cancelWhenShowing() {
+        if (mStopMillisecond < mStartMillisecond + DELAY_MILLISECOND.toLong() + SHOW_MIN_MILLISECOND.toLong()) {
+            val handler = Handler()
+            handler.postDelayed({ dismissAllowingStateLoss() }, SHOW_MIN_MILLISECOND.toLong())
+        } else {
+            dismissAllowingStateLoss()
+        }
+    }
+
+    private fun cancelWhenNotShowing() {
+        val handler = Handler()
+        handler.postDelayed({ dismissAllowingStateLoss() }, SHOW_MIN_MILLISECOND.toLong())
+    }
+
+    companion object {
+        private val DELAY_MILLISECOND = 450
+        private val SHOW_MIN_MILLISECOND = 300
+
+        fun newInstance(message: String): ProgressDialog {
+            val instance = ProgressDialog()
+
+            val arguments = Bundle()
+            arguments.putString("message", message)
+            instance.arguments = arguments
+
+            return instance
+        }
+    }
+
+    fun move() {
+        mProgressBar?.max = 3
+        val thread = Thread(Runnable {
+            try {
+                mProgressBar?.progress = 1
+                Thread.sleep(1000)
+                mProgressBar?.progress = 2
+            } catch (e: Exception) {
+            }
+        })
+        thread.start()
+    }
+}
--- app/src/main/java/com/example/nakatsuka/newgit/navigationAction/StampFragment.kt
+++ app/src/main/java/com/example/nakatsuka/newgit/navigationAction/StampFragment.kt
@@ -3,17 +3,16 @@ package com.example.nakatsuka.newgit.navigationAction
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
+import com.example.nakatsuka.newgit.R
 import com.example.nakatsuka.newgit.mainAction.APITest
 import com.example.nakatsuka.newgit.mainAction.SecondActivity
-import com.example.nakatsuka.newgit.R
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_stamp.*
 
 
 class StampFragment : Fragment() {
@@ -21,64 +20,87 @@ class StampFragment : Fragment() {
     val RESULT_SUBACTIVITY: Int = 1000
     private val buttonResult = mutableListOf<Boolean>(false, false, false, false, false, false)
 
-
-
-
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.fragment_stamp, container, false)
+        val view = inflater.inflate(R.layout.fragment_stamp, container, false)
+
+        //ダイアログが出ます
+        val event = View.OnClickListener {
+            val mProgressDialog = ProgressDialog.newInstance("ビーコン取得中...")
+            mProgressDialog.setTargetFragment(this, 100)
+            mProgressDialog.show(activity!!.supportFragmentManager, "dialog")
+            val thread = Thread(Runnable {
+                try {
+                    Thread.sleep(1000)
+                    mProgressDialog.move()
+                } catch (e: Exception) {
+                }
+                try {
+                    Thread.sleep(2000)
+                } catch (e: Exception) {
+                }
+                mProgressDialog!!.dismiss()
+            })
+            thread.start()
+        }
+        view.findViewById<Button>(R.id.imageButton1).setOnClickListener(event)
+        view.findViewById<Button>(R.id.imageButton2).setOnClickListener(event)
+        view.findViewById<Button>(R.id.imageButton3).setOnClickListener(event)
+        view.findViewById<Button>(R.id.imageButton4).setOnClickListener(event)
+        view.findViewById<Button>(R.id.imageButton5).setOnClickListener(event)
+        view.findViewById<Button>(R.id.imageButton6).setOnClickListener(event)
+
+        return view
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-/**
+        /**
 
         Log.d("firstdesu","firstdesu")
         imageButton1.setOnClickListener {
-            val quizNumber = 0
-            goActivity(quizNumber)
+        val quizNumber = 0
+        goActivity(quizNumber)
         }
         Log.d("firstafter","firstafter")
         imageButton2.setOnClickListener {
-            val quizNumber = 1
-            goActivity(quizNumber)
+        val quizNumber = 1
+        goActivity(quizNumber)
         }
         imageButton3.setOnClickListener {
-            val quizNumber = 2
-            goActivity(quizNumber)
+        val quizNumber = 2
+        goActivity(quizNumber)
         }
         imageButton4.setOnClickListener {
-            val quizNumber = 3
-            goActivity(quizNumber)
+        val quizNumber = 3
+        goActivity(quizNumber)
         }
         imageButton5.setOnClickListener {
-            val quizNumber = 4
-            goActivity(quizNumber)
+        val quizNumber = 4
+        goActivity(quizNumber)
         }
         imageButton6.setOnClickListener {
-            val quizNumber = 5
-            goActivity(quizNumber)
+        val quizNumber = 5
+        goActivity(quizNumber)
         }
 
         for(i in 0..5){
-            if(buttonResult[i]){
-                when(i){
-                    0 -> imageButton1.setImageResource(0)
-                    1 -> imageButton2.setImageResource(0)
-                    2 -> imageButton3.setImageResource(0)
-                    3 -> imageButton4.setImageResource(0)
-                    4 -> imageButton5.setImageResource(0)
-                    5 -> imageButton6.setImageResource(0)
-                }
-            }
+        if(buttonResult[i]){
+        when(i){
+        0 -> imageButton1.setImageResource(0)
+        1 -> imageButton2.setImageResource(0)
+        2 -> imageButton3.setImageResource(0)
+        3 -> imageButton4.setImageResource(0)
+        4 -> imageButton5.setImageResource(0)
+        5 -> imageButton6.setImageResource(0)
         }
-*/
+        }
+        }
+         */
     }
-
 
 
     private fun goActivity(answerNumber: Int) {
@@ -99,18 +121,15 @@ class StampFragment : Fragment() {
     }
 
 
-
     private fun makeToast(message: String, x: Int, y: Int) {
         val toast: Toast = Toast.makeText(activity, message, Toast.LENGTH_SHORT)
         toast.setGravity(Gravity.CENTER, x, y / 4)
         toast.show()
     }
 
-    fun setState(state:Boolean,num:Int){
+    fun setState(state: Boolean, num: Int) {
         buttonResult[num] = state
     }
-
-
 
 }
 
