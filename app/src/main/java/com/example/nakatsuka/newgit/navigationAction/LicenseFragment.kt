2e7a1bf83222530db233aeb5674681e464078cf7
Wyutaka <tihiro.haul@gmail.com>
2018-11-17 23:08:38 +0900

    ライセンスの追加

--- app/src/main/java/com/example/nakatsuka/newgit/navigationAction/LicenseFragment.kt
+++ app/src/main/java/com/example/nakatsuka/newgit/navigationAction/LicenseFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,10 +24,13 @@ class LicenseFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        //shut_license.setOnClickListener {
-
-        //}
-
         return inflater.inflate(R.layout.fragment_license, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        shut_license.setOnClickListener {
+            fragmentManager!!.beginTransaction().remove(this).commit()
+        }
     }
 }
